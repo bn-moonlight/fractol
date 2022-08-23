6c8bb7ff7369646d8b25c78f0e1c39ac3801b619
Sedef Akkaya <sakkaya@k2m12s01.42kocaeli.com.tr>
2022-08-23 17:50:32 +0300

    a

---
+++ inc/key_macos_en.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_macos_en.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakkaya <sakkaya@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 19:23:28 by tjensen           #+#    #+#             */
+/*   Updated: 2022/08/20 14:48:26 by sakkaya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEY_MACOS_EN_H
+# define KEY_MACOS_EN_H
+
+# define ARROW_LEFT			123
+# define ARROW_RIGHT		124
+# define ARROW_UP			126
+# define ARROW_DOWN			125
+
+# define MOUSE_UP			4
+# define MOUSE_DOWN			5
+
+# define EXIT_ESC			53
+# define MOVE_SPACE			49
+# define COLOR_PLUS			24
+# define COLOR_MINUS		27
+# define RESET_R			15
+# define CHANGE_FUNC_C		8
+# define KEY_1				18
+# define KEY_2				19
+# define KEY_3				20
+# define KEY_4				21
+
+#endif
---
+++ src/fractal_mandelbrot.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal_mandelbrot.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakkaya <sakkaya@student.42kocaeli.com.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 13:05:51 by tjensen           #+#    #+#             */
+/*   Updated: 2022/08/19 09:17:50 by sakkaya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	mandelbrot(t_fractol *frctl)
+{
+	t_complex	z;
+	t_complex	tmp;
+
+	z.re = frctl->c.re;
+	z.im = frctl->c.im;
+	tmp.re = z.re * z.re;
+	tmp.im = z.im * z.im;
+	frctl->iter = 0;
+	while ((tmp.re + tmp.im <= 8) && frctl->iter < frctl->max_iter)
+	{
+		z.im = 2 * z.re * z.im + frctl->c.im;
+		z.re = tmp.re - tmp.im + frctl->c.re;
+		tmp.re = z.re * z.re;
+		tmp.im = z.im * z.im;
+		frctl->iter++;
+	}
+}
